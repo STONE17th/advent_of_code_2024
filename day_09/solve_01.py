307200783a25639b1a3db025765b3e1150347ecf
STONE <17th@inbox.ru>
2024-12-12 11:00:35 +0300

    day_09_02

--- day_09/solve_01.py
+++ day_09/solve_01.py
@@ -1,29 +1,56 @@
 from itertools import chain
 
 
-def read_data(path):
-    with open(path, 'r') as file:
-        return file.read().strip()
+class Cell:
+
+    def __init__(self, data: bool, cell_id: int = 0):
+        self.data = data
+        self.id = cell_id
+
+    def __repr__(self):
+        return str(self.id) if self.data else '.'
+
+
+class Section:
+    section_id = 0
+
+    def __init__(self, data: bool, count: str):
+        self.count = int(count)
+        self.data = data
+        if data:
+            self.data = Section.section_id
+            Section.section_id += 1
+        self.section = [Cell(data, self.data) for i in range(self.count)]
+
+    def __repr__(self):
+        return ''.join(map(str, self.section))
+
+
+class DiskMap:
+    def __init__(self, path: str):
+        with open(path, 'r') as file:
+            data = file.read().strip()
+        self.map = [Section(not bool(i % 2), item) for i, item in enumerate(data)]
+
+    def list_disk(self) -> list[Cell]:
+        return list(chain(*[section.section for section in self.map]))
+
+    def __str__(self):
+        return ''.join(map(str, self.map))
 
 
 def solution(path: str):
-    data = read_data(path)
-    data_ext = []
-    for i, item in enumerate(data):
-        data_ext.append((i // 2 if not i % 2 else '.', int(item)))
-    data_ext = list(chain(*[[str(item[0])] * item[1] for item in data_ext]))
-    # print(data_ext)
-    ife = len(data_ext) - 1
-    for ifs in range(len(data_ext)):
-        while not data_ext[ife].isdigit():
+    disk_map = DiskMap(path)
+    list_disk = disk_map.list_disk()
+    ife = len(list_disk) - 1
+    for ifs in range(len(list_disk)):
+        while not list_disk[ife].data:
             ife -= 1
         if ifs > ife:
             break
-        if data_ext[ifs] == '.':
-            data_ext[ifs], data_ext[ife] = data_ext[ife], data_ext[ifs]
-
-    print(''.join(data_ext))
-    return sum([i * int(item) for i, item in enumerate(data_ext) if item.isdigit()])
+        if not list_disk[ifs].data:
+            list_disk[ifs], list_disk[ife] = list_disk[ife], list_disk[ifs]
+    return sum([cell.id * new_id for new_id, cell in enumerate(list_disk) if cell.data])
 
 
 print(solution('input_data.txt'))
--- day_09/solve_02.py
+++ day_09/solve_02.py
@@ -1,49 +1,103 @@
 from itertools import chain
 
+from itertools import chain
 
-def read_data(path):
-    with open(path, 'r') as file:
-        return file.read().strip()
+
+class Cell:
+
+    def __init__(self, data: str | bool):
+        self.data = data
+
+    def __repr__(self):
+        return self.data if self.data else '.'
+
+
+class Section:
+    section_id = 0
+
+    def __init__(self, section_id: str, count: int):
+        self.id = str(section_id)
+        self.count = count
+        self.section = [Cell(self.id) for _ in range(self.count)]
+
+    def __repr__(self):
+        return ''.join(map(str, self.section))
+
+    def __copy__(self):
+        instance = Section(self.id, self.count)
+        return instance
+
+    def __len__(self):
+        return len(self.section)
+
+
+class DiskMap:
+    section_id = 0
+
+    def __init__(self, path: str):
+        with open(path, 'r') as file:
+            data = file.read().strip()
+        self.map = []
+        for i, item in enumerate(data):
+            if int(item):
+                if not i % 2:
+                    data = DiskMap.section_id
+                    DiskMap.section_id += 1
+                else:
+                    data = ''
+                self.map.append(Section(data, int(item)))
+
+    def list_disk(self) -> list[Cell]:
+        return list(chain(*[section.section for section in self.map]))
+
+    def swap_section(self, data_id: int, empty_id: int):
+        data_section = self.map.copy()[data_id]
+        empty_section = self.map.copy()[empty_id]
+        len_data = len(data_section)
+        len_empty = len(empty_section)
+        difference = len_data - len_empty
+        self.map[data_id] = empty_section
+        self.map[empty_id] = Section('', len_empty)
+        shift = True
+        if difference:
+            new_section = Section('', difference)
+            self.map.insert(data_id + 1, new_section)
+            shift = False
+        return shift
+
+    def sum_cells(self):
+        result = 0
+        k = 0
+        for section in self.map:
+            for cell in section.section:
+                if cell.data:
+                    result += int(cell.data) * k
+                k += 1
+        return result
+
+    def __str__(self):
+        return ''.join(map(str, self.map))
 
 
 def solution(path: str):
-    data = read_data(path)
-    data_ext = []
-    for i, item in enumerate(data):
-        data_ext.append((i // 2 if not i % 2 else '.', int(item)))
-    data_ext = list(chain(*[[str(item[0])] * item[1] for item in data_ext]))
-    new_data = []
-    row = [data_ext[0]]
-    for i in range(1, len(data_ext)):
-        if row[-1] == data_ext[i]:
-            row.append(data_ext[i])
+    disk_map = DiskMap(path)
+    ife = len(disk_map.map) - 1
+    while ife >= 0:
+        ifs = 0
+        if disk_map.map[ife].id:
+            while ifs < ife:
+                if (not disk_map.map[ifs].id) and (len(disk_map.map[ifs]) >= len(disk_map.map[ife])):
+                    if disk_map.swap_section(ifs, ife):
+                        ife -= 1
+                    break
+                else:
+                    ifs += 1
+            else:
+                ife -= 1
+                ifs += 1
         else:
-            new_data.append(row)
-            row = [data_ext[i]]
-    new_data.append(row)
-    i_digit = len(new_data) - 1
-    while i_digit >= 0:
-        i_dots = 0
-        while i_dots < len(new_data):
-            shift = 0
-            if i_digit < i_dots:
-                break
-            if new_data[i_dots][0] == '.' and len(new_data[i_digit]) <= len(new_data[i_dots]):
-                len_digits = len(new_data[i_digit])
-                len_dots = len(new_data[i_dots])
-
-                difference = len_dots - len_digits
-                new_data[i_dots] = new_data[i_digit].copy()
-                if difference:
-                    new_data.insert(i_dots + 1, ['.'] * difference)
-                    shift = 1
-                new_data[i_digit + shift] = ['.'] * len_digits
-                break
-
-            i_dots += 1
-        i_digit -= 1
-    print(new_data)
-    return sum([i * int(item) for i, item in enumerate(chain(*new_data)) if item != '.'])
+            ife -= 1
+    return disk_map.sum_cells()
 
 
 print(solution('input_data.txt'))
